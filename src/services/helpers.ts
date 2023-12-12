817614cab9ebf009b1b32020f78e85f4ab7da765
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-12 08:27:42 +0200

    correct date in new doc

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -1,11 +1,18 @@
 export const transformDateFormat = (inputDate: string) => {
-  const parts = inputDate.split("-");
+  const parts = inputDate.includes("-")
+    ? inputDate.split("-")
+    : inputDate.split(" ");
 
-  const date = new Date(`${parts[2]}-${parts[0]}-${parts[1]}`);
+  const date = inputDate.includes("-")
+    ? new Date(`${parts[2]}-${parts[0]}-${parts[1]}`)
+    : new Date(`${parts[2]}-${parts[1]}-${parts[3]}`);
 
   const options:{} = { day: "numeric", month: "long", year: "numeric" };
 
   const formattedDate = date.toLocaleDateString("en-GB", options);
+
+console.log(formattedDate);
+
 
   return formattedDate;
 };
